a08568d461ed58ae51167ab6383d63e87cae42b3
SergeiLonsh <132091994+SergeiLonsh@users.noreply.github.com>
2023-06-08 23:24:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int [] randomarray = new int [4];
+
+        int i=0;
+while (i< randomarray.Length)
+{
+   randomarray[i] = new Random().Next(100,1000);
+      
+Console.Write($"{randomarray[i]} ");  
+i++; 
+}
+
+     int pos = 0;
+     int res;    
+    while (i < randomarray.Length)
+     {
+        res = randomarray[i] % 10 % 2;
+
+        if ( res == 0)
+        {
+           pos = pos + 1;
+        }
+        else 
+        pos = pos;
+    i++;
+        
+     }
+Console.Write("=> "+pos);
